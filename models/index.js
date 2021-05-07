4527f48c0a165bb5b1488846a9ec8ffd08297ece
Vincent Vo <itsvnvo@gmail.com>
2021-05-06 22:57:39 -0500

    Added all models database types etc, fixed index.js for now

--- models/index.js
+++ models/index.js
@@ -5,13 +5,34 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  through: {
+    model: Tag,
+    unique: false
+  }
+})
 // Categories have many Products
+Category.hasMany(Product, {
+  through: {
+    model: Tag,
+    unique: false
+  }
+})
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  }
+})
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  }
+})
 module.exports = {
   Product,
   Category,
