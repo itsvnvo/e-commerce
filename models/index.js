2a495aa715587b8fcaf01dfb0923e2d7e08361dd
Vincent Vo <itsvnvo@gmail.com>
2021-05-07 01:45:26 -0500

    changed category_name to product_name in product.js, added routes for category and products

--- models/index.js
+++ models/index.js
@@ -6,32 +6,22 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  through: {
-    model: Tag,
-    unique: false
-  }
+  foreignKey: 'category_id'
 })
 // Categories have many Products
 Category.hasMany(Product, {
-  through: {
-    model: Tag,
-    unique: false
-  }
+  foreignKey: 'category_id'
 })
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: {
-    model: ProductTag,
-    unique: false
-  }
+  through: ProductTag,
+  foreignKey: 'product_id',
 })
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: {
-    model: ProductTag,
-    unique: false
-  }
+  through: ProductTag,
+  foreignKey: 'tag_id'
 })
 module.exports = {
   Product,
